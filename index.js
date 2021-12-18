f338c03446037a9771fe80aaec02b267b7ec5608
Treinamento Unidavi <treinamento@UNIDAVI.local>
2021-12-18 09:57:29 -0300

    Atualiza

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import PrismaClientPkg from "@prisma/client";
 import express from "express";
 import { dirname } from 'path';
-import  path  from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 //Constantes
@@ -10,6 +10,26 @@ const prisma = new PrismaClient();
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 //Rotas
@@ -29,7 +49,7 @@ app.get('/api/noticias', async (req, res) => {
 });
 
 app.get('/api/noticias/:id', async (req, res) => {
-    const users = await prisma.noticia.findUnique({where: {codigo: parseInt(req.params.id)}});
+    const users = await prisma.noticia.findUnique({ where: { codigo: parseInt(req.params.id) } });
     users.image = req.get('host') + '/images/' + users.image;
     res.send(users);
 
@@ -37,6 +57,6 @@ app.get('/api/noticias/:id', async (req, res) => {
 
 
 //Inicializa Server
-app.listen('80', '10.4.240.83');
+app.listen('80');
 
 
